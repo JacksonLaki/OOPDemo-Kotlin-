92cb5854131e93f10777c521bbaf36051c0b31f1
JacksonLaki <jacksonlazarocs@gmail.com>
2024-04-22 03:06:37 -0700

    ObjectOrientedProgramming(Kotlin)

--- app/src/main/java/com/example/oopdemo/MyCar.kt
+++ app/src/main/java/com/example/oopdemo/MyCar.kt
@@ -2,9 +2,17 @@ package com.example.oopdemo
 
 import android.util.Log
 
-class MyCar:Car() {
+class MyCar:Car(), SpeedController{
     override fun start() {
      Log.i("MyTag","This is MyCar Starting...")
+    }
+
+    override fun accelerate() {
+
+    }
+
+    override fun decelerate() {
+
     }
 
 }
--- app/src/main/java/com/example/oopdemo/SpeedController.kt
+++ app/src/main/java/com/example/oopdemo/SpeedController.kt
@@ -2,4 +2,11 @@ package com.example.oopdemo
 
 interface SpeedController {
 
+   fun  accelerate()
+   fun  decelerate()
+
+   fun getBrandId():String{
+
+       return "ABS1212"
+   }
 }
